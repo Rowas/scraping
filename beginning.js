998c440f360e247df15ecc383f2e5cfc9d74cd1e
Rowas <defenderzann@gmail.com>
2014-05-16 12:29:06 +0200

    Clear up some ... training scripts.

--- beginning.js
+++
@@ -1,6 +0,0 @@
-var request = require('request');
-var cheerio = require('cheerio');
-
-var url = 'http://api.arbetsformedlingen.se/platsannons/matchning?lan
-
-	$ = cheerio.load('<listnamn="lan">...</listnamn>');
